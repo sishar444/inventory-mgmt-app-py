c88062bae3533593581d3dc4b8545141394c9d33
Sahil Ishar <sahil.ishar@mastercard.com>
2018-06-11 18:58:25 -0400

    Updated to use real api

--- products_app/app.py
+++ products_app/app.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import requests
+import json
 
 def menu(username="@prof-rossetti", products_count=100):
     # this is a multi-line string, also using preceding `f` for string interpolation
@@ -46,7 +48,7 @@ def reset_products_file(filename="products.csv", from_filename="products_default
     write_products_to_file(filename, products)
 
 def run():
-    # First, read products from file...
+    # # First, read products from file...
     products = read_products_from_file()
 
     # Then, prompt the user to select an operation...
@@ -55,24 +57,23 @@ def run():
 
     # Then, handle selected operation: "List", "Show", "Create", "Update", "Destroy" or "Reset"...
     if operation.title() == "List":
-        list_products(products)
+        porducts = list_products()
     elif operation.title() == "Show":
-        show_product(products)
+        show_product()
     elif operation.title() == "Create":
         id = get_largest_product_id(products) if len(products) > 0 else 0
-        products.append(create_product_with_id(id+1))
+        create_product_with_id(id+1)
     elif operation.title() == "Update":
-        products = update_product(products)
+        update_product()
     elif operation.title() == "Destroy":
-        # Overwrite products list with new list
-        products = destroy_product(products)
+        destroy_product()
     elif operation.title() == "Reset":
         reset_products_file()
         return
     else:
         print("Sorry, that operation was not recognized.")
     # Finally, save products to file so they persist after script is done...
-    write_products_to_file(products=products)
+    # write_products_to_file(products=products)
 
 #
 # Helper functions
@@ -96,24 +97,40 @@ def get_largest_product_id(products):
 #
 # List Products
 #
-def list_products(products):
-    header_string = f"--------------------------------\nLISTING {len(products)} PRODUCTS:\n--------------------------------"
-    print(header_string)
-    for product in products:
-        print(" #" + product["id"] + ":", product["name"])
+def list_products():
+    request_url = "https://nyu-info-2335-products-api-csv.herokuapp.com/products"
+    response = requests.get(request_url)
+
+    products = []
+    try:
+        products = json.loads(response.text)
+        header_string = f"--------------------------------\nLISTING {len(products)} PRODUCTS:\n--------------------------------"
+        print(header_string)
+        for product in products:
+            print(" #" + str(product["id"]) + ":", product["name"])
+    except ValueError:
+        print("Invalid response. Please try again.")
+
+    return products
 
 #
 # Show Product
 #
-def show_product(products):
+def show_product():
     product_id = input("Ok. Please specify the product's identifier: ")
-    product_list = product_by_id(product_id, products)
-    if len(product_list) > 0:
+
+    request_url = "https://nyu-info-2335-products-api-csv.herokuapp.com/products/" + product_id
+    response = requests.get(request_url)
+
+    try:
+        product_list = json.loads(response.text)
+        # If we reach here, api returned a product
         header_string = "--------------------------------\nSHOWING A PRODUCT:\n--------------------------------"
         print(header_string)
-        print(product_as_dict(product_list))
-    else:
+        print(product_list)
+    except ValueError:
         print("There is no product with that identifier.")
+
 
 #
 # Create Product
@@ -123,52 +140,73 @@ def create_product_with_id(id):
     product_aisle = input("Ok. Please input the product's 'aisle': ")
     product_dept = input("Ok. Please input the product's 'department': ")
     product_price = input("Ok. Please input the product's 'price': ")
-    header_string = "--------------------------------\nCREATING A NEW PRODUCT:\n--------------------------------"
-    print(header_string)
+
     product_dict = {"id": id, "name": product_name, "aisle": product_aisle, "department": product_dept, "price": product_price}
-    print(product_dict)
-    return product_dict
+    request_url = "https://nyu-info-2335-products-api-csv.herokuapp.com/products"
+    response = requests.post(request_url, json=product_dict)
+
+    try:
+        response_data = json.loads(response.text)
+        header_string = "--------------------------------\nCREATING A NEW PRODUCT:\n--------------------------------"
+        print(header_string)
+        print(response_data)
+    except ValueError:
+        print("Invalid response. Please try again.")
+
 
 #
 # Update Product
 #
-def update_product(products):
+def update_product():
     product_id = input("Ok. Please specify the product's identifier: ")
-    prod_list = product_by_id(product_id, products)
-    if len(prod_list) > 0:
-        prod_dict = product_as_dict(prod_list)
-        product_name = input(f"Ok. What is the product's new 'name' (currently '{prod_dict['name']}'): ")
-        product_aisle = input(f"Ok. What is the product's new 'aisle' (currently '{prod_dict['aisle']}'): ")
-        product_dept = input(f"Ok. What is the product's new 'department' (currently '{prod_dict['department']}'): ")
-        product_price = input(f"Ok. What is the product's new 'price' (currently '{prod_dict['price']}'): ")
-        header_string = "--------------------------------\nUPDATING A PRODUCT:\n--------------------------------"
-        print(header_string)
-        product_dict = {"id": product_id, "name": product_name, "aisle": product_aisle, "department": product_dept, "price": product_price}
-        print(product_dict)
-        products = [product_dict if prod.get('id') == product_id else prod for prod in products]
-    else:
-        print("There is no product with that identifier.")
 
-    return products
+    request_url = "https://nyu-info-2335-products-api-csv.herokuapp.com/products/" + product_id
+    response = requests.get(request_url)
+
+    try:
+        product_list = json.loads(response.text)
+        # If we reach here, api returned a product
+        product_name = input(f"Ok. What is the product's new 'name' (currently '{product_list['name']}'): ")
+        product_aisle = input(f"Ok. What is the product's new 'aisle' (currently '{product_list['aisle']}'): ")
+        product_dept = input(f"Ok. What is the product's new 'department' (currently '{product_list['department']}'): ")
+        product_price = input(f"Ok. What is the product's new 'price' (currently '{product_list['price']}'): ")
+
+        product_dict = {"id": product_id, "name": product_name, "aisle": product_aisle, "department": product_dept, "price": product_price}
+
+        request_url = "https://nyu-info-2335-products-api-csv.herokuapp.com/products/" + product_id
+        response = requests.put(request_url, json=product_dict)
+
+        try:
+            response_data = json.loads(response.text)
+            header_string = "--------------------------------\nUPDATING A PRODUCT:\n--------------------------------"
+            print(header_string)
+            print(response_data)
+        except ValueError:
+            print("Invalid response. Please try again.")
+    except ValueError:
+        print("There is no product with that identifier.")
 
 #
 # Destroy Product
 #
-def destroy_product(products):
+def destroy_product():
     product_id = input("Ok. Please specify the product's identifier: ")
-    # Print the details of product to be deleted
-    product_list = product_by_id(product_id, products)
-    if len(product_list) > 0:
-        header_string = "--------------------------------\nDESTROYING A PRODUCT:\n--------------------------------"
-        print(header_string)
-        print(product_as_dict(product_list))
-        # Update passed in list to remove product with that id
-        products = [prod for prod in products if prod.get('id') != product_id]
-    else:
+
+    request_url = "https://nyu-info-2335-products-api-csv.herokuapp.com/products/" + product_id
+    response = requests.delete(request_url)
+
+    try:
+        product_list = json.loads(response.text)
+        # Check if response is an error message
+        if product_list["message"]:
+            print(product_list["message"])
+        else:
+            header_string = "--------------------------------\nDESTROYING A PRODUCT:\n--------------------------------"
+            print(header_string)
+            print(product_list)
+    except ValueError:
         print("There is no product with that identifier.")
 
-    # Return the new list
-    return products
 
 # only prompt the user for input if this script is run from the command-line
 # this allows us to import and test this application's component functions
